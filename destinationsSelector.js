a756755b3914010cfcde2252e11c12ee17d24731
Rick Marshall <isben22@gmail.com>
2020-10-20 14:46:33 -0400

    completed functions

--- destinationsSelector.js
+++ destinationsSelector.js
@@ -1,26 +1,53 @@
 //Requirement: destination to be randomly selected for my day trip.
 
 //destination function
-// function destination() {
-//     let destinations = ["Athens", "Chania", "Marathon", "Mykanos", "Santorini"]; // create an array of desitnations that will be picked from
-//     let randomNumber = Math.floor(Math.random() * destinations.length); //generate random number between 0 and 4 (length - 1)
-//     let finalDestination = destinations[randomNumber];
+function destination() {
+    let destinations = ["Athens", "Chania", "Marathon", "Mykanos", "Santorini"]; // create an array of desitnations that will be picked from
+    let randomNumber = Math.floor(Math.random() * destinations.length); //generate random number between 0 and 4 (length - 1)
+    let finalDestination = destinations[randomNumber];
 
-// return finalDestination;
-// }
+return finalDestination;
+}
    
-// console.log = (destination);
+let theSpot =destination();
 
 
 //restaurant function
 
 function restaurant() {
-    let restaurant = ["Nikkos", "Thes", "Akrogiali", "Noa"]; //set number to 0-3 to change it up
+    let restaurant = ["Nikkos", "Thes", "Akrogiali", "Noa"]; //set number to 0-3 
     let randomNumber = Math.floor(Math.random() * restaurant.length);
     let finalRestaurant = restaurant[randomNumber];
 
 return finalRestaurant;
 }
 
-let hungry =restaurant();
-console.log (hungry); //this errored out start debug at first function
+let hungry=restaurant();
+
+//transportation function
+
+function transportation() {
+    let transportation = ["Taxi", "Plane", "Uber", "Donkey", "Foot"]; 
+    let randomNumber = Math.floor(Math.random() * transportation.length);
+    let finalTransportation = transportation[randomNumber];
+
+return finalTransportation;
+}
+
+let mode=transportation();
+
+//entertainment function
+function entertainment() {
+    let entertainment = ["Oedipus Rex", "Medea", "The Persians", "The Oddesey", "Cyclops"]; 
+    let randomNumber = Math.floor(Math.random() * entertainment.length);
+    let finalEntertainment = entertainment[randomNumber];
+
+return finalEntertainment;
+}
+
+let show=entertainment();
+
+
+let dayTrip = ("You will take a " + mode + " to visit " + hungry + " in the city of " + theSpot + " and enjoy the show " + show +"!" );//concantonates into a human readable sentenace
+console.log(dayTrip);
+
