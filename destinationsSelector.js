055d14ff8257c07c1530b3dc28136de80a9fdfe2
Rick Marshall <isben22@gmail.com>
2020-10-20 12:09:24 -0400

    fixed restaurant

--- destinationsSelector.js
+++ destinationsSelector.js
@@ -1,15 +1,15 @@
 //Requirement: destination to be randomly selected for my day trip.
 
 //destination function
-function destination() {
-    let destinations = ["Athens", "Chania", "Marathon", "Mykanos", "Santorini"]; // create an array of desitnations that will be picked from
-    let randomNumber = Math.floor(Math.random() * destinations.length); //generate random number between 0 and 4 (length - 1)
-    let finalDestination = destinations[randomNumber];
+// function destination() {
+//     let destinations = ["Athens", "Chania", "Marathon", "Mykanos", "Santorini"]; // create an array of desitnations that will be picked from
+//     let randomNumber = Math.floor(Math.random() * destinations.length); //generate random number between 0 and 4 (length - 1)
+//     let finalDestination = destinations[randomNumber];
 
-return finalDestination;
-}
-    
-console.log (destination);
+// return finalDestination;
+// }
+   
+// console.log = (destination);
 
 
 //restaurant function
@@ -22,4 +22,5 @@ function restaurant() {
 return finalRestaurant;
 }
 
-console.log (restaurant); //this errored out start debug at first function
+let hungry =restaurant();
+console.log (hungry); //this errored out start debug at first function
