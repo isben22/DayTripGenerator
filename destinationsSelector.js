88beb2d724d25d5730a75738e09e58b8285a6a00
Rick Marshall <isben22@gmail.com>
2020-10-19 23:12:29 -0400

    updated funcitons

--- destinationsSelector.js
+++ destinationsSelector.js
@@ -1,14 +1,25 @@
 //Requirement: destination to be randomly selected for my day trip.
-// create an array of desitnations that will be picked from
-function destination() {
-    let destinations = ["Athens", "Chania", "Marathon", "Mykanos", "Santorini"];
-    //let finalDestination =""; //I want to move the selection to out of the choices to preserve for another trip
 
-    for ( let i = 0; i < destinations.length; i++){ //here is where I want to loop through the string and choose a random index and return that items to selected Destination. 
-        
-    finalDestination = destinations.charAt(Math.floor(Math.random() * destinations.length));
-    }
+//destination function
+function destination() {
+    let destinations = ["Athens", "Chania", "Marathon", "Mykanos", "Santorini"]; // create an array of desitnations that will be picked from
+    let randomNumber = Math.floor(Math.random() * destinations.length); //generate random number between 0 and 4 (length - 1)
+    let finalDestination = destinations[randomNumber];
+
 return finalDestination;
 }
     
 console.log (destination);
+
+
+//restaurant function
+
+function restaurant() {
+    let restaurant = ["Nikkos", "Thes", "Akrogiali", "Noa"]; //set number to 0-3 to change it up
+    let randomNumber = Math.floor(Math.random() * restaurant.length);
+    let finalRestaurant = restaurant[randomNumber];
+
+return finalRestaurant;
+}
+
+console.log (restaurant);
