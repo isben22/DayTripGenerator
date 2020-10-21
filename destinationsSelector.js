b6229230f0f4dfbb8415c5beff2b207b819a9b45
Rick Marshall <isben22@gmail.com>
2020-10-21 15:09:54 -0400

    first working copy

--- destinationsSelector.js
+++ destinationsSelector.js
@@ -1,8 +1,6 @@
 //Requirement: destination to be randomly selected for my day trip.
 //given direction to collapse functions into single nested funciton - tony S.
 
-
-
     //destination function
         function destination() {
             let destinations = ["Athens", "Chania", "Marathon", "Mykanos", "Santorini"]; // create an array of desitnations that will be picked from
@@ -12,7 +10,7 @@
     }
     //restaurant function
         function restaurant() {
-            let restaurant = ["Nikkos", "Thes", "Akrogiali", "Noa"]; //set number to 0-3 
+            let restaurant = ["Nikkos", "Thes", "Akrogiali", "Noa"]; 
             let randomNumber = Math.floor(Math.random() * restaurant.length);
             let finalRestaurant = restaurant[randomNumber];
     return finalRestaurant;
@@ -31,14 +29,8 @@
             let finalEntertainment = entertainment[randomNumber];
     return finalEntertainment;
     }
-    
-   
-    // As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
-    // approach: 1. offer input for user to run selection. 2. have user select which portion they want to run (or all), 3. push selection/s into string
-    //without pushing this to the web page (yet) after string is pushed to console; ask user in a prompt if they are satisfied with selection. 
-    //fun if/ statement that if !=true, then offere reselection function; else end. 
-
-   function dayTrip(){ 
+        //concatenation function
+    function dayTrip(){ 
         let finalDestination = destination();
         let finalRestaurant = restaurant();
         let finalTransportation = transportation();
@@ -46,19 +38,23 @@
         let dayTrip = ("You will take a " + finalTransportation + " to visit " + finalRestaurant + " in the city of " + finalDestination + " and enjoy the show " + finalEntertainment +"!" );//concantonates into a human readable sentenace
     return dayTrip; 
    }
-   let firstTrip = dayTrip();
+      //validation function - created a function to prompt user if they are happy. 
+function validation(){
+    let validation = prompt("Are you happy with your choices?");
+    return validation;
+}
+  
+let firstTrip = dayTrip(); 
    console.log(firstTrip);
 
-   let choice = prompt("Are you happy with your choices?");  //take it from here
-   while (choice = 'no'){
-   dayTrip();
-   console.log(firstTrip);
-   choice
+   while(validation() == "no"){
+    firstTrip = dayTrip(); // update trip with new itinerary
+    console.log(firstTrip);
    }
+   
+   console.log("Your trip has been confirmed, enjoy!");
 
-//    let triplog = dayTrip();
-//    console.log(triplog);
-
+//---------------------------------- variance in requirement interpretation see notes-------------------//
 
 // below- I was attempting to present the user the option of randomly selecting one aspect of the trip. Discussed with **David L. **and he said for the scope of the problem, 
 // I could feed the user a while loop and a randomly generated string until the condition was true. 
