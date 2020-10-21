6a1956c4674625cc58f5617db06b976d6058afed
Rick Marshall <isben22@gmail.com>
2020-10-21 15:22:06 -0400

    Final with markup

--- destinationsSelector.js
+++ destinationsSelector.js
@@ -1,35 +1,34 @@
-//Requirement: destination to be randomly selected for my day trip.
 //given direction to collapse functions into single nested funciton - tony S.
-
-    //destination function
+    //R9:  I want all of my functions to have a Single Responsibility.
+    //destination function -  R1: I want a destination to be randomly selected for my day trip.
         function destination() {
             let destinations = ["Athens", "Chania", "Marathon", "Mykanos", "Santorini"]; // create an array of desitnations that will be picked from
             let randomNumber = Math.floor(Math.random() * destinations.length); //generate random number between 0 and 4 (length - 1)
             let finalDestination = destinations[randomNumber];
         return finalDestination;
     }
-    //restaurant function
+    //restaurant function - R2:  restaurant to be randomly selected for my day trip.
         function restaurant() {
             let restaurant = ["Nikkos", "Thes", "Akrogiali", "Noa"]; 
             let randomNumber = Math.floor(Math.random() * restaurant.length);
             let finalRestaurant = restaurant[randomNumber];
     return finalRestaurant;
     }
-    //transportation function
+    //transportation function - R3: mode of transportation to be randomly selected for my day trip
         function transportation() {
             let transportation = ["Taxi", "Plane", "Boat", "Donkey", "Skateboard"]; 
             let randomNumber = Math.floor(Math.random() * transportation.length);
             let finalTransportation = transportation[randomNumber];
     return finalTransportation;
     }
-    //entertainment function
+    //entertainment function - R4:  form of entertainment to be randomly selected for my day trip
         function entertainment() {
             let entertainment = ["Oedipus Rex", "Medea", "The Persians", "The Oddesey", "Cyclops"]; 
             let randomNumber = Math.floor(Math.random() * entertainment.length);
             let finalEntertainment = entertainment[randomNumber];
     return finalEntertainment;
     }
-        //concatenation function
+        //concatenation function human readable output
     function dayTrip(){ 
         let finalDestination = destination();
         let finalRestaurant = restaurant();
@@ -38,9 +37,11 @@
         let dayTrip = ("You will take a " + finalTransportation + " to visit " + finalRestaurant + " in the city of " + finalDestination + " and enjoy the show " + finalEntertainment +"!" );//concantonates into a human readable sentenace
     return dayTrip; 
    }
-      //validation function - created a function to prompt user if they are happy. 
+      //validation function - R5:  I want to be able to randomly re-select a destination, restaurant, mode of transportation, 
+      //and/or form of entertainment if I don’t like one or more of those things 
 function validation(){
-    let validation = prompt("Are you happy with your choices?");
+    let validation = prompt("Are you happy with your choices?"); //R6: able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things
+                                                                 //R7: I want to be able to confirm that my day trip is “complete” once I like all of the random selections
     return validation;
 }
   
@@ -49,7 +50,7 @@ let firstTrip = dayTrip();
 
    while(validation() == "no"){
     firstTrip = dayTrip(); // update trip with new itinerary
-    console.log(firstTrip);
+    console.log(firstTrip); //R8: I want to display my completed trip in the console.
    }
    
    console.log("Your trip has been confirmed, enjoy!");
